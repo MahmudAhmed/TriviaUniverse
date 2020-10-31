8818444353e8bdf019708b5aa551b3849aaebc79
Mo <57582938+MahmudAhmed@users.noreply.github.com>
2020-10-31 01:50:26 -0400

    escaped html

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import './stylesheets/reset.css';
-import './stylesheets/App.css';
-import QUESTIONS from './Apprentice_TandemFor400_Data.json';
-import DisplayAnswers from "./components/display_answers"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar, faBrain, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faPlayCircle } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons'
-import './stylesheets/categories.css';
+import React from "react";
+import "./stylesheets/reset.css";
+import "./stylesheets/App.css";
+import QUESTIONS from "./Apprentice_TandemFor400_Data.json";
+import DisplayAnswers from "./components/display_answers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faBrain, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium
+} from "@fortawesome/free-brands-svg-icons";
+import "./stylesheets/categories.css";
 const axios = require("axios");
 
-
 const CATAGORIES = [
-  { "id": 1, "category": "Tandem: Random (Max: 21)"},
-  { "id": 9, "category": "General Knowledge"},
-  { "id": 10, "category": "Entertainment: Books"},
-  { "id": 11, "category": "Entertainment: Films"},
-  { "id": 12, "category": "Entertainment: Music"},
-  { "id": 14, "category": "Entertainment: Television"},
-  { "id": 15, "category": "Entertainment: Video Games"},
-  { "id": 17, "category": "Entertainment: Science & Nature"},
-  { "id": 20, "category": "Mythology"},
-  { "id": 21, "category": "Sports"},
-  { "id": 22, "category": "Geography"},
-  { "id": 23, "category": "History"},
-  { "id": 24, "category": "Politics"},
-  { "id": 25, "category": "Art"},
-  { "id": 26, "category": "Celebrities"},
-]
+  { id: 1, category: "Tandem: Random (Max: 21)" },
+  { id: 9, category: "General Knowledge" },
+  { id: 10, category: "Entertainment: Books" },
+  { id: 11, category: "Entertainment: Films" },
+  { id: 12, category: "Entertainment: Music" },
+  { id: 14, category: "Entertainment: Television" },
+  { id: 15, category: "Entertainment: Video Games" },
+  { id: 17, category: "Entertainment: Science & Nature" },
+  { id: 31, category: "Entertainment: Japanese Anime & Manga" },
+  { id: 20, category: "Mythology" },
+  { id: 21, category: "Sports" },
+  { id: 22, category: "Geography" },
+  { id: 23, category: "History" },
+  { id: 24, category: "Politics" },
+  { id: 26, category: "Celebrities" }
+];
 
-const QUESTIONS_AMOUNT = [10, 20, 30, 40, 50]
+const QUESTIONS_AMOUNT = [10, 20, 30, 40, 50];
+const DIFFICULTIES = ["easy", "medium", "hard"];
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -41,7 +44,8 @@ class App extends React.Component {
       questionAmount: 10,
       questions: [],
       categorySelection: true,
-      highScore: parseInt(localStorage.getItem('triviaScore') || 0),
+      difficulty: "",
+      highScore: parseInt(localStorage.getItem("triviaScore") || 0)
     };
   }
 
@@ -50,18 +54,26 @@ class App extends React.Component {
   // }
 
   fetchQuestions = () => {
-    const { categoryId, questionAmount } = this.state; 
-    if ( categoryId === 1) {
+    const { categoryId, questionAmount, difficulty } = this.state;
+    if (categoryId === 1) {
       const shuffled = QUESTIONS.sort(() => 0.5 - Math.random());
       let amount = questionAmount >= 21 ? 21 : questionAmount;
       let tenQuestion = shuffled.slice(0, amount);
-      this.setState({questions: tenQuestion})
+      this.setState({ questions: tenQuestion });
     } else {
-      axios.get(`https://opentdb.com/api.php?amount=${questionAmount}&category=${categoryId}`).then((data) => {
-        this.setState({ questions: data.data.results });
-      });
+      axios
+        .get(
+          `https://opentdb.com/api.php?amount=${questionAmount}&category=${categoryId}${
+            difficulty ? `&difficulty=${difficulty}` : ""
+          }`
+        )
+        .then((data) => {
+          debugger;
+          console.log(data.data.results);
+          this.setState({ questions: data.data.results });
+        });
     }
-  }
+  };
 
   displayQuestions = () => {
     const { questions, idx } = this.state;
@@ -70,11 +82,20 @@ class App extends React.Component {
     return (
       <div className="question-container">
         <div className="the-question">
-          <h2>{question.question}</h2>
+          <h2 dangerouslySetInnerHTML={{ __html: `${question.question}` }}></h2>
         </div>
-        <DisplayAnswers questions={questions} idx={idx} handleAnswerClk={this.handleAnswerClk} />
+        <DisplayAnswers
+          questions={questions}
+          idx={idx}
+          handleAnswerClk={this.handleAnswerClk}
+        />
         <div className="quit-container">
-          <FontAwesomeIcon id="trash" title="Start Over" icon={faTrash} onClick={() => this.setState({ categorySelection: true})}/>
+          <FontAwesomeIcon
+            id="trash"
+            title="Start Over"
+            icon={faTrash}
+            onClick={() => this.setState({ categorySelection: true })}
+          />
         </div>
       </div>
     );
@@ -82,23 +103,19 @@ class App extends React.Component {
 
   handleAnswerClk = (questions, idx, choice) => {
     const question = questions[idx];
-    debugger
-    let {
-      score,
-      gameOver,
-      highScore,
-    } = this.state;
+    debugger;
+    let { score, gameOver, highScore } = this.state;
 
     if (question.correct_answer === choice) {
-      score += 1;     
+      score += 1;
     } else {
-      score--; 
+      score--;
     }
     if (idx + 1 >= questions.length) {
       gameOver = true;
       if (score > highScore) {
         highScore = score;
-        localStorage.setItem('triviaScore', highScore)
+        localStorage.setItem("triviaScore", highScore);
       }
     } else {
       idx++;
@@ -107,7 +124,7 @@ class App extends React.Component {
     this.setState({
       score,
       idx,
-      gameOver, 
+      gameOver,
       highScore
     });
   };
@@ -121,50 +138,63 @@ class App extends React.Component {
             <FontAwesomeIcon id="brain" icon={faBrain} />
             <h1 className="title">TRIVIA UNIVERSITY</h1>
           </div>
-          {
-            !categorySelection ? (
+          {!categorySelection ? (
             <div className="right-title-container">
               <div className="score-container">
                 <FontAwesomeIcon id="star" icon={faStar} />
                 <span id="score">{score}</span>
               </div>
-              <div><span>|</span></div>
+              <div>
+                <span>|</span>
+              </div>
               <div className="pagination">
-                <h5># {idx + 1} / {questions.length}</h5>
+                <h5>
+                  # {idx + 1} / {questions.length}
+                </h5>
               </div>
             </div>
-            ) : ""
-          }
+          ) : (
+            ""
+          )}
         </div>
         <div className="middle-container">
-          { categorySelection ? this.categorySelection() : ( !gameOver ? this.displayQuestions() : this.displayGameOver()) } 
-        </div>            
+          {categorySelection
+            ? this.categorySelection()
+            : !gameOver
+            ? this.displayQuestions()
+            : this.displayGameOver()}
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
   displayGameOver = () => {
     return (
-    <div className="game-over-container">
-      <h2 id="game-over-text">Play Again!</h2>
-      <div className="replay-container">
-        <FontAwesomeIcon id="restart" title="Play Again" icon={faPlayCircle} onClick={ () => this.setState({ categorySelection: true})}/>
-      </div>
-      <div className="stats-container">
-        <span>You Scored:</span>
-        <span id="stats-score">{this.state.score}</span>
-        {
-          this.state.highScore > 0 ? (
+      <div className="game-over-container">
+        <h2 id="game-over-text">Play Again!</h2>
+        <div className="replay-container">
+          <FontAwesomeIcon
+            id="restart"
+            title="Play Again"
+            icon={faPlayCircle}
+            onClick={() => this.setState({ categorySelection: true })}
+          />
+        </div>
+        <div className="stats-container">
+          <span>You Scored:</span>
+          <span id="stats-score">{this.state.score}</span>
+          {this.state.highScore > 0 ? (
             <div>
               <span>Your Highest Score:</span>
               <span id="stats-score">{this.state.highScore}</span>
             </div>
-          ) : ""
-        }
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-    </div>
     );
-  }
+  };
 
   newGame = () => {
     this.fetchQuestions();
@@ -172,15 +202,24 @@ class App extends React.Component {
       gameOver: false,
       score: 0,
       idx: 0,
-      questionAmount: 10,
-    })
-  }
+      questionAmount: 10
+    });
+  };
 
   handleRadioClick = (e) => {
     this.setState({
       questionAmount: parseInt(e.currentTarget.value)
-    })
-  }
+    });
+  };
+
+  handleDifficultySelection = (e) => {
+    this.setState(
+      {
+        difficulty: e.currentTarget.value
+      },
+      () => console.log(this.state.difficulty)
+    );
+  };
 
   categorySelection = () => {
     const { questionAmount } = this.state;
@@ -188,51 +227,81 @@ class App extends React.Component {
       <div className="category-container">
         <h2 id="category-title">Select A Category to Begin</h2>
         <ul className="category-list">
-          {
-            CATAGORIES.map( (topic) => {
-              return (
-              <li className="categories" onClick={ () => {
-                this.setState({ categorySelection: false, categoryId: topic.id}, () => this.newGame());              
-              }}>
+          {CATAGORIES.map((topic) => {
+            return (
+              <li
+                className="categories"
+                onClick={() => {
+                  this.setState(
+                    { categorySelection: false, categoryId: topic.id },
+                    () => this.newGame()
+                  );
+                }}
+              >
                 {topic.category}
               </li>
-              )
-            })
-          }
+            );
+          })}
         </ul>
         <div className="amount-container">
           <h2>How many questions would you like?</h2>
           <div className="amount-list">
-            { QUESTIONS_AMOUNT.map( (num, idx) => {
+            {QUESTIONS_AMOUNT.map((num, idx) => {
               return (
                 <label>
-                  <input type="radio" name="amount"
+                  <input
+                    type="radio"
+                    name="amount"
                     value={num}
                     checked={questionAmount === num}
-                    onChange={ this.handleRadioClick } 
-                  />  
+                    onChange={this.handleRadioClick}
+                  />
                   <span class="amount">{num}</span>
                 </label>
-              )
-            }) }
+              );
+            })}
           </div>
         </div>
+        {this.displayDifficulty()}
       </div>
     );
-};
+  };
 
+  displayDifficulty = () => {
+    return (
+      <div>
+        <label for="difficulty">Choose a difficulty:</label>
+        <select
+          name="dificulty"
+          id="difficulty-list"
+          onChange={this.handleDifficultySelection}
+        >
+          <option value="">Any Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+    );
+  };
 
   render() {
     return (
       <div className="main-container">
-       {this.displayGameScreen()}
-       <div class="social-icon-container">
-               <a href="https://www.linkedin.com/in/mahmud-ahmed/" target="_black"><FontAwesomeIcon className="social-icons" icon={faLinkedin} /></a>
-               <a href="https://github.com/mahmudahmed" target="_black"><FontAwesomeIcon className="social-icons" icon={faGithub} /></a>
-               <a href="https://medium.com/@moe.purplefox" target="_black"><FontAwesomeIcon className="social-icons" icon={faMedium} /></a>
+        {this.displayGameScreen()}
+        <div class="social-icon-container">
+          <a href="https://www.linkedin.com/in/mahmud-ahmed/" target="_black">
+            <FontAwesomeIcon className="social-icons" icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/mahmudahmed" target="_black">
+            <FontAwesomeIcon className="social-icons" icon={faGithub} />
+          </a>
+          <a href="https://medium.com/@moe.purplefox" target="_black">
+            <FontAwesomeIcon className="social-icons" icon={faMedium} />
+          </a>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/display_answers.jsx
+++ src/components/display_answers.jsx
@@ -1,19 +1,25 @@
-function DisplayAnswers({questions, idx, handleAnswerClk}) {
-    const question = questions[idx];
-    const choices = question.incorrect_answers.slice();
-    choices.push(question.correct_answer);
-    choices.sort(() => 0.5 - Math.random());
-    return (
-      <div className="answers-container">
-        <ul className="choices-ul">
-          {choices.map((option, key) => (
-            <li key={key} className="choices-li">
-              <button className="answer-btns" onClick={() => handleAnswerClk(questions, idx, option)}>{option}</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
+import React from "react";
+
+function DisplayAnswers({ questions, idx, handleAnswerClk }) {
+  const question = questions[idx];
+  const choices = question.incorrect_answers.slice();
+  choices.push(question.correct_answer);
+  choices.sort(() => 0.5 - Math.random());
+  return (
+    <div className="answers-container">
+      <ul className="choices-ul">
+        {choices.map((option, key) => (
+          <li key={key} className="choices-li">
+            <button
+              className="answer-btns"
+              onClick={() => handleAnswerClk(questions, idx, option)}
+              dangerouslySetInnerHTML={{ __html: `${option}` }}
+            ></button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default DisplayAnswers;
